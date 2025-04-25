b72e59ca0523de88bb8cf913d316f0f1dea800b3
mismyst <nayanstarkt@gmail.com>
2025-04-25 18:24:46 +0530

    Initial commit

---
+++ src/test/java/com/example/mutualfollowers/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.mutualfollowers;  // Change this from com.example.demo
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutualFollowersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
